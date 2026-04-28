30e2183d4a6369ba382e7bd307241c79ae5506b4
AlaaTaher007 <alaatah2007er@gmail.com>
2026-04-28 02:59:27 +0200

    fix: repair supabase connection and admin buttons

--- env.py
+++
@@ -1,6 +0,0 @@
-SUPABASE_URL = "https://fvdapblrzlacyrmyimma.supabase.co"
-SUPABASE_KEY = "sb_publishable_dwmFkkAsMCRR9aNU-R7aww_boGHUgLi"
-SECRET_KEY = "final_ultra_key_2026"
-
-ADMIN_USER = "admin123"
-ADMIN_PASS = "654321"
--- main.py
+++ main.py
@@ -228,4 +228,4 @@ def logout():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)    
